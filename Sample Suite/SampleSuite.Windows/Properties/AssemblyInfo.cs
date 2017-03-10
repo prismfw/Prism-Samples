ec818a78ce22a99c18d76674aef2a25d3bd992ba
jonathan-git <jonathan-git@users.noreply.github.com>
2017-03-10 00:39:43 -0600

    Updated copyright to 2017.

--- Sample Suite/SampleSuite.Windows/Properties/AssemblyInfo.cs
+++ Sample Suite/SampleSuite.Windows/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Prism Framework Team")]
 [assembly: AssemblyProduct("SampleSuite.Windows")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
